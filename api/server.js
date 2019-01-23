6333eb4250549b431876eb24772a13a5fcab7a92
BrandCam <BrandCam>
2019-01-23 17:23:39 -0600

    added endpoint to create a user and encrypted the pass

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
-
+const bcrypt = require("bcryptjs");
 const knexConfig = require("../knexfile.js");
 
 const server = express();
@@ -13,5 +13,24 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.status(200).json({ message: "get working" });
 });
+
+// create user
+server.post("/create", (req, res) => {
+  const userInfo = req.body;
+  const hash = bcrypt.hashSync(userInfo.password, 12);
+  userInfo.password = hash;
+  db("users")
+    .insert(userInfo)
+    .then(ids => {
+      res.status(200).json({ ids });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// login as user
+
+// get user list if logged in
 
 module.exports = server;
